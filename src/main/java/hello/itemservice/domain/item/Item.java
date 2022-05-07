6607d22e5dd0efadf0fcd038c190fed132871bb4
username <user@email.mail>
2022-05-07 18:54:24 +0900

    commit

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,30 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10,000원 넘게 입력해주세요.")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
+
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}) // 수정 시에만  적용
     private String itemName;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class) //등록 시에만 적용
     private Integer quantity;
 
     public Item() {
